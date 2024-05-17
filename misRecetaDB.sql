85565fefe66a456ac9fa3356b4edd0ba8797a0a3
Paula_Blazquez <paula.blazquez@estudiant.fjaverianas.com>
2024-05-17 16:34:08 +0200

    bd

--- misRecetaDB.sql
+++ misRecetaDB.sql
@@ -12,12 +12,23 @@ CREATE TABLE IF NOT EXISTS usuarios (
     password VARCHAR(100)
 );
 
+INSERT INTO `usuarios` (`id`, `nombre`,`email`, `password`) VALUES
+(1, 'albert', 'wew@wwe.com','admin'),
+(2, 'paula', 'Paulablazquez@gmail.com' ,'1234');
+
 -- Crea la tabla de recetas
 CREATE TABLE IF NOT EXISTS recetas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(255),
-    fecha DATE,
+    fecha text,
     tiempo TEXT,
     ingredientes TEXT,
     preparacion TEXT
 );
+
+
+INSERT INTO `recetas` (`id`, `titulo`,`fecha`, `tiempo`,`ingredientes`, `preparacion` ) VALUES
+(1, 'Carbonara', '2000-05-17','30min', 'huevo, pasta, sal, pimienta, parmesano', 'hervir la pasta, batir una yema de huevo, rallar el queso, salpimentar'),
+(2, 'Guisantes con jamon', '2000-05-17' ,'1h', 'guisantes, jamon a trozos', 'hervir los guisantes, freir el jamon, juntar todo');
+
+
